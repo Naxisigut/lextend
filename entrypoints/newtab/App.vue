ecebc7b586d69242668ecb3a7095e3e4f2e6c169
“lcc” <chengcheng.li@51hgp.com>
2024-10-11 13:47:23 +0800

    Initial commit

---
+++ entrypoints/newtab/App.vue
@@ -0,0 +1,11 @@
+﻿<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="WXT + Vue New Tab" />
+</template>
+
+<style scoped>
+/* 可以添加任何特定于新标签页的样式 */
+</style>
