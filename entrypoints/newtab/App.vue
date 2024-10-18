3d1e11a7e566b4a09e498333b494c96fd5c34089
“lcc” <chengcheng.li@51hgp.com>
2024-10-18 18:52:32 +0800

    feat: 引入shadcn

--- entrypoints/newtab/App.vue
+++ entrypoints/newtab/App.vue
@@ -1,10 +1,13 @@
 ﻿<script lang="ts" setup>
 import HelloWorld from '@/components/HelloWorld.vue';
+
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
   <div>
-    <HelloWorld />
+    <button>Click me</button>
+    <Button>Click me</Button>
   </div>
 </template>
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,26 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, presetUno } from 'unocss'
+import presetAnimations from 'unocss-preset-animations';
+import { presetShadcn} from 'unocss-preset-shadcn';
 
 export default defineConfig({
   // UnoCSS options
+  presets: [
+    presetUno(),
+    presetAnimations(),
+    presetShadcn({
+      // color: 'red',
+      // With default setting for SolidUI, you need to set the darkSelector option.
+      darkSelector: '[data-kb-theme="dark"]',
+    }),
+  ],
+  content: {
+    pipeline: {
+      include: [
+        // the default
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        '(components|src)/**/*.{js,ts}',
+      ],
+    },
+  },
 })
