8d8ef53d6c6f22581d59086188c92041b0b50a1a
Naxisigut <834525217@qq.com>
2024-10-26 17:20:57 +0800

    feat: 网站快捷入口

---
+++ components/newtab/DisplayItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <Card>
+    <a :href="item.href" target="_self">
+      <CardHeader>
+        <CardTitle>{{ item.title }}</CardTitle>
+        <CardDescription>{{ item.desc }}</CardDescription>
+      </CardHeader>
+    </a>
+  </Card>
+</template>
+
+<script lang="ts" setup>
+
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardTitle,
+  CardDescription,
+} from '@/components/ui/card'
+
+defineProps({
+  item: {
+    type: Object,
+    required: true
+  }
+})
+</script>
--- entrypoints/newtab/App.vue
+++ entrypoints/newtab/App.vue
@@ -1,15 +1,36 @@
-﻿<script lang="ts" setup>
-import HelloWorld from '@/components/HelloWorld.vue';
-
-import { Button } from '@/components/ui/button'
-</script>
-
-<template>
+﻿<template>
   <div>
-    <button>Click me</button>
-    <Button>Click me</Button>
+    <div class="container mx-auto">
+      <div class="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-3">
+        <DisplayItem v-for="item, idx in items" :key="idx" :item="item"></DisplayItem>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-</style>
+<script lang="ts" setup>
+import { reactive } from 'vue';
+import DisplayItem from '@/components/newtab/DisplayItem.vue';
+
+const items = reactive([
+  {
+    title: 'UnoCSS',
+    desc: 'UnoCSS',
+    href: 'http://unocss.cn/'
+  },
+  {
+    title: 'Shadcn Vue',
+    desc: 'Shadcn Vue',
+    href: 'https://www.shadcn-vue.com/docs/components/accordion.html'
+  },
+  {
+    title: 'TailWindCSS',
+    desc: 'TailWindCSS',
+    href: 'https://www.tailwindcss.cn/docs/installation'
+  },
+])
+
+</script>
+
+
+<style scoped lang="scss"></style>
