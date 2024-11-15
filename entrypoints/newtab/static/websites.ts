01bdfcee47891d6015741860641633d74eb231fc
“lcc” <chengcheng.li@51hgp.com>
2024-11-15 21:55:13 +0800

    update

--- entrypoints/newtab/static/websites.ts
+++ entrypoints/newtab/static/websites.ts
@@ -12,13 +12,18 @@ const General: WebsiteItem[] = [
   },
   {
     title: 'Github Repos',
-    desc: 'Github Repos',
+    desc: 'Github仓库',
     href: 'https://github.com/Naxisigut?tab=repositories'
   },
   {
     title: 'ChatGPT',
     desc: 'ChatGPT',
     href: 'https://chatgpt.com/'
+  },
+  {
+    title: 'Cursor',
+    desc: 'Cursor',
+    href: 'https://www.cursor.com/settings'
   },
   {
     title: 'Google Translate',
@@ -30,12 +35,42 @@ const General: WebsiteItem[] = [
     desc: '翻墙',
     href: 'https://star.369.cyou/' 
   },
+  {
+    title: 'Regexr',
+    desc: '正则表达式测试',
+    href: 'https://regexr.com/' 
+  },
+  {
+    title: 'Excalidraw',
+    desc: '流程图',
+    href: 'https://excalidraw.com/' 
+  },
+  {
+    title: 'Snippet Generator',
+    desc: 'vscode代码片段',
+    href: 'https://snippet-generator.app/?description=&tabtrigger=&snippet=&mode=vscode' 
+  },
+  {
+    title: 'vscode 快捷键',
+    desc: 'vscode 快捷键',
+    href: 'https://www.dute.org/vscode-shortcut' 
+  },
+  {
+    title: 'CSS Inspiration',
+    desc: '个人博客 chokcoco',
+    href: 'https://chokcoco.github.io/CSS-Inspiration/#/' 
+  },
+  {
+    title: '字体编辑',
+    desc: 'iconfont字体编辑',
+    href: 'https://www.1json.com/front/fonteditor.html' 
+  },
 ]
 
 const FrontEnd: WebsiteItem[] = [
   {
     title: 'UnoCSS',
-    desc: 'UnoCSS官方文档',
+    desc: 'css框架',
     href: 'http://unocss.cn/config/'
   },
   {
@@ -45,12 +80,12 @@ const FrontEnd: WebsiteItem[] = [
   },
   {
     title: 'Shadcn Vue',
-    desc: 'Shadcn Vue',
+    desc: '无头组件库',
     href: 'https://www.shadcn-vue.com/docs/components/accordion.html'
   },
   {
     title: 'TailWindCSS',
-    desc: 'TailWindCSS',
+    desc: 'css框架',
     href: 'https://www.tailwindcss.cn/docs/installation'
   },
   {
@@ -88,21 +123,56 @@ const FrontEnd: WebsiteItem[] = [
     desc: 'Vue3 hook',
     href: 'https://vueuse.org/guide/'
   },
-  // {
-  //   title: 'vue-sonner3',
-  //   desc: 'sonner toast组件',
-  //   href: 'https://vue-sonner.vercel.app/'
-  // },
-  // {
-  //   title: 'vue-sonner4',
-  //   desc: 'sonner toast组件',
-  //   href: 'https://vue-sonner.vercel.app/'
-  // },
-  // {
-  //   title: 'vue-sonner5',
-  //   desc: 'sonner toast组件',
-  //   href: 'https://vue-sonner.vercel.app/'
-  // },
+  {
+    title: 'sortablejs',
+    desc: '拖拽排序',
+    href: 'https://sortablejs.com/'
+  },
+  {
+    title: 'vue-treeselect',
+    desc: '树形下拉',
+    href: 'https://vue-treeselect.js.org/'
+  },
+  {
+    title: 'rollup.js',
+    desc: '构建 打包工具',
+    href: 'https://www.rollupjs.com/'
+  },
+  {
+    title: 'mescroll',
+    desc: '移动端滚动组件',
+    href: 'https://www.mescroll.com/api.html'
+  },
+  {
+    title: 'TS入门教程',
+    desc: '电子书',
+    href: 'https://ts.xcatliu.com/'
+  },
+  {
+    title: 'Next.js',
+    desc: 'React框架',
+    href: 'https://nextjs.org/docs'
+  },
+  {
+    title: 'Vitest',
+    desc: '测试框架 vue3',
+    href: 'https://cn.vitest.dev/guide/'
+  },
+  {
+    title: 'UniBest',
+    desc: 'Uniapp框架 vue3',
+    href: 'https://codercup.github.io/unibest-docs/base/1-introduction'
+  },
+  {
+    title: 'Ant Design Vue',
+    desc: 'Vue组件库',
+    href: 'https://2x.antdv.com/components/input-cn' 
+  },
+  {
+    title: 'Flowbite Icon',
+    desc: '无头图标库',
+    href: 'https://flowbite.com/icons/' 
+  },
 ]
 
 const Resource: WebsiteItem[] = [
