b023248c23b6047688372b2bddf41995ca6bb698
Naxisigut <834525217@qq.com>
2025-06-27 16:30:22 +0800

    feat: 分组增删改

--- entrypoints/newtab/views/WebSites.vue
+++ entrypoints/newtab/views/WebSites.vue
@@ -7,7 +7,7 @@
         </LiquidGlassWrapper>
       </div>
       <div v-for="group in allData" :key="group.groupId" class="space-y-4">
-        <h2 class="text-xl font-semibold text-gray-700 dark:text-gray-300">{{ group.groupName }}</h2>
+        <h2 class="text-xl font-semibold text-gray-700 dark:text-gray-300" @contextmenu.prevent="onGroupContextMenu($event, group)">{{ group.groupName }}</h2>
         <div class="grid grid-cols-[repeat(auto-fill,minmax(120px,1fr))] auto-rows-[120px] ">
           <LiquidGlassWrapper v-for="website in group.data" :key="website.id" class=" hover:scale-120 hover:z-10" @contextmenu.prevent="onContextMenu($event, website, group)">
             <a :href="website.href" target="_blank"
@@ -22,7 +22,7 @@
             </a>  
           </LiquidGlassWrapper>
           <LiquidGlassWrapper class="hover:scale-120 hover:z-10">
-            <div class="h-full flex items-center justify-center" @click="onOpen('addNewWebsite', group.groupId)">
+            <div class="h-full flex items-center justify-center" @click="onPlus(group.groupId)">
               <Plus class="w-10 h-10 text-white"/>
             </div>
           </LiquidGlassWrapper>
@@ -32,17 +32,17 @@
   </div>
 
   
-  <Dialog title="添加类型" v-model:open="isOpen" @confirm="onConfirm">
+  <Dialog :title="dialogTitle" v-model:open="isOpen" @confirm="onConfirm">
     <div class="p-4 space-y-4">
-      <template v-if="openType === 'addNewGroup'">
+      <template v-if="openType === 'addNewGroup' || openType === 'editGroup'">
         <Input type="text" v-model="newGroupName" placeholder="请输入类型名称" />
       </template>
-      <template v-if="openType === 'addNewWebsite'">
+      <template v-if="openType === 'addNewWebsite' || openType === 'editWebsite'">
         <Input type="text" v-model="newWebsite.title" placeholder="请输入名称" />
         <Input type="text" v-model="newWebsite.desc" placeholder="请输入描述" />
         <Input type="text" v-model="newWebsite.href" placeholder="请输入链接地址" />
         <Input type="text" v-model="newWebsite.icon" placeholder="请输入图标地址" />
-        <Select v-model="newWebsite.typeId" disabled>
+        <Select v-model="newWebsite.typeId" :disabled="openType === 'addNewWebsite'">
           <SelectTrigger>
             <SelectValue placeholder="请选择类型" />
           </SelectTrigger>
@@ -63,7 +63,13 @@ import { Input } from "@/components/ui/input"
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
 import request from '@/lib/http';
 import ContextMenu from '@imengyu/vue3-context-menu'
-type DialogOpenType = 'addNewGroup' | 'addNewWebsite' | ''
+
+// #region 类型定义
+type DialogOpenType = '' 
+  | 'addNewWebsite' 
+  | 'editWebsite' 
+  | 'addNewGroup'
+  | 'editGroup'
 interface Website {
   id: string
   title: string
@@ -77,26 +83,79 @@ interface WebsiteGroup {
   groupName: string
   data: Website[]
 }
+// #endregion
 
+
+const newGroupName = ref('')
+const newWebsite = ref<Website>({
+  id: '',
+  title: '',
+  desc: '',
+  href: '',
+  icon: '',
+  typeId: ''
+})
+const currGroupId = ref('')
+const currItem = ref<Website | null>(null)
+const onPlus = (groupId: string) => {
+  currGroupId.value = groupId
+  onOpen('addNewWebsite')
+}
+
+//#region 弹窗
+const dialogTitle = ref('')
 const isOpen = ref(false)
 const openType = ref<DialogOpenType>('')
-const onOpen = (type: DialogOpenType, groupId?: string) => {
+const onOpen = (type: DialogOpenType) => {
   openType.value = type
-  if(groupId){
-    currGroupId.value = groupId
-    newWebsite.value.typeId = groupId
+  if(type === 'addNewGroup'){
+    newGroupName.value = ""
+    dialogTitle.value = "新增分组"
+  }
+  if(type === 'editGroup'){
+    const currGroup = allData.value.find((i) => i.groupId === currGroupId.value)
+    if(!currGroup)return
+    newGroupName.value = currGroup.groupName
+    dialogTitle.value = "编辑分组"
+  }
+  if(type === 'addNewWebsite'){
+    newWebsite.value = {
+      id: '',
+      title: '',
+      desc: '',
+      href: '',
+      icon: '',
+      typeId: currGroupId.value || '' // 新建时固定类型
+    }
+    dialogTitle.value = "新增网站"
+  }
+
+  if(type === 'editWebsite'){
+    const item = currItem.value
+    if(!item)return
+    newWebsite.value = {
+      ...item, 
+      typeId: item.typeId.toString()
+    }
+    dialogTitle.value = "编辑网站"
   }
   isOpen.value = true
 }
 const onConfirm = () => {
   if(openType.value === 'addNewGroup'){
     onConfirmAddNewGroup()
+  }else if(openType.value === 'editGroup'){
+    onConfirmEditGroup()
   }else if(openType.value === 'addNewWebsite'){
     onConfirmAddNewWebsite()
+  }else if(openType.value === 'editWebsite'){
+    onConfirmEditWebsite()
   }
 }
-/* 新建类型 */
-const newGroupName = ref('')
+//#endregion
+
+
+//#region 新建分组
 const onConfirmAddNewGroup = () => {
   if (!newGroupName.value) return
   request.post('/AddWebsiteType', {
@@ -104,24 +163,46 @@ const onConfirmAddNewGroup = () => {
   }).then((res) => {
     if(res.success){
       isOpen.value = false
-      newGroupName.value = ''
+      initWebsiteData()
     }else{
       // 获取失败 msg
     }
   })
 }
+//#endregion
+
+//#region 编辑分组
+const onConfirmEditGroup = () => {
+  if (!newGroupName.value) return
+  request.post('/UpdateWebsiteType', {
+    id: currGroupId.value,
+    name: newGroupName.value
+  }).then((res) => {
+    if(res.success){
+      isOpen.value = false
+      initWebsiteData()
+    }else{
+      // 获取失败 msg
+    }
+  })
+}
+//#endregion
+
+//#region 删除分组
+const onDeleteGroup = async (groupId: string) => {
+  const deleteRes = await request.post('/DeleteWebsiteType', {
+    id: groupId
+  })
+  if(!deleteRes.success) {
+    // 删除失败msg
+    return
+  }
+  initWebsiteData()
+}
+//#endregion
 
 
-
-const currGroupId = ref('')
-/* 新建网站 */
-const newWebsite = ref({
-  title: '',
-  desc: '',
-  href: '',
-  icon: '',
-  typeId: ''
-})
+//#region 新建网站
 const onConfirmAddNewWebsite = async () => {
   // fake validate
   if (!newWebsite.value.title) return
@@ -132,17 +213,25 @@ const onConfirmAddNewWebsite = async () => {
     return
   }
   refreshGroupDataByTypeId(currGroupId.value)
-  newWebsite.value = {
-    title: '',
-    desc: '',
-    href: '',
-    icon: '',
-    typeId: ''
-  }
   isOpen.value = false
 }
+// #endregion
 
-/* 删除网站 */
+//#region 编辑网站
+const onConfirmEditWebsite = () => {
+  if (!newWebsite.value.title) return
+  request.post('/UpdateWebsite', newWebsite.value).then((res) => {
+    if(res.success){
+      refreshGroupDataByTypeId(currGroupId.value)
+      isOpen.value = false
+    }else{
+      // 获取失败 msg
+    }
+  })
+}
+//#endregion
+
+//#region 删除网站
 const onDeleteWebsite = async (website: Website) => {
   const deleteRes = await request.post('/DeleteWebsite', {
     id: website.id
@@ -153,38 +242,75 @@ const onDeleteWebsite = async (website: Website) => {
   }
   refreshGroupDataByTypeId(currGroupId.value)
 }
-/* 编辑网站 */
-const onEditWebsite = (website: Website) => {
-  console.log(website);
+// #endregion
+
+//#region 右键菜单
+const defaultMenuData = {
+  theme: "mac",
+  // menuTransitionProps: {
+  //   duration: 1000,
+  //   name: 'fade'
+  // },
+}
+const contextMenuItemAction = (label: MenuLabelType | MenuGroupLabelType) => {
+  ContextMenu.closeContextMenu()
+  switch (label) {
+    case '编辑':
+      onOpen('editWebsite')
+      break;
+    case '删除':
+      if (!currItem.value) return
+      onDeleteWebsite(currItem.value)
+      break;
+    case '新增分组':
+      onOpen('addNewGroup')
+      break;
+    case '编辑分组':
+      onOpen('editGroup')
+      break;
+    case "删除分组":
+      onDeleteGroup(currGroupId.value)
+      break;
+    default:
+      break;
+  }
 }
 
 
+const menuLabels = ['编辑', '删除'] as const
+type MenuLabelType = typeof menuLabels[number]
 const onContextMenu = (e: MouseEvent, website: Website, group: WebsiteGroup) => {
   currGroupId.value = group.groupId
+  currItem.value = website
   ContextMenu.showContextMenu({
-    theme: "mac",
-    // menuTransitionProps: {
-    //   name: 'fade'
-    // },
-    x: e.clientX,
-    y: e.clientY,
-    items: [
-      {
-        label: '编辑',
-        onClick: () => onEditWebsite(website)
-      },
-      {
-        label: '删除',
-        onClick: () => onDeleteWebsite(website)
-      },
-    ]
+    ...defaultMenuData,
+    items: menuLabels.map(label => ({
+      label,
+      onClick: () => contextMenuItemAction(label)
+    })),
+    x: e.x,
+    y: e.y,
   })
 }
 
+const menuGroupLabels = ['新增分组', '编辑分组', '删除分组'] as const
+type MenuGroupLabelType = typeof menuGroupLabels[number]
+const onGroupContextMenu = (e: MouseEvent, group: WebsiteGroup) => {
+  currGroupId.value = group.groupId
+  ContextMenu.showContextMenu({
+    ...defaultMenuData,
+    items: menuGroupLabels.map(label => ({
+      label,
+      onClick: () => contextMenuItemAction(label)
+    })),
+    x: e.x,
+    y: e.y,
+  })
+}
 
+//#endregion
 
-
-/* 获取所有数据 */
+// #region 初始化
 /* getAllTypes => 根据类型获取 */
 const websiteTypes = ref<{id: string, text: string}[]>([])
 const allData = ref<WebsiteGroup[]>([])
@@ -226,6 +352,8 @@ const initWebsiteData = async () => {
 }
 initWebsiteData()
 
+//#endregion
+
 </script>
 
 <style lang="scss" scoped>
@@ -242,4 +370,13 @@ initWebsiteData()
     opacity: 1;
   }
 }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
 </style>
